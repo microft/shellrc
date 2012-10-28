ee281a12a8acf2a1efe05a2808ff9335115ea6ab
Luis Miguel Braga <human@microft.org>
2012-10-28 14:51:32 +0000

    removed bash specific loader

--- loader.bash
+++
@@ -1,8 +0,0 @@
-# This sources all files in the BASHRC variable, if they indeed exist
-#echo $BASHRCDIR
-for b in ${BASHRC[@]}; do
-    if [ -f $BASHRCDIR$b ]; then
-      source $BASHRCDIR$b
-    fi  
-done
-

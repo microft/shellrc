aea537a708d7fd002ccee3fbc60d97fcbeef9c23
Luis Miguel Braga <luismiguel@silvabraga.com>
2013-04-12 15:27:18 +0100

    some sorting of repeated configs

--- base.zsh
+++ base.zsh
@@ -3,3 +3,16 @@
 ACTIVE_SHRC='BASE'
 export ACTIVE_SHRC
 
+# Emulate tcsh's backward-delete-word
+tcsh-backward-delete-word () {
+    #local WORDCHARS="${WORDCHARS:s#/#}"
+    #local WORDCHARS='*?_[]~\/!#$%^<>|`@#$%^*()+?'
+    local WORDCHARS="${WORDCHARS:s#/#}"
+    zle backward-delete-word
+}
+
+WORDCHARS="${WORDCHARS:s#/#}"
+#zle -N tcsh-backward-delete-word
+# for escape backspace (delete a word) behavior similar to tcsh
+#bindkey '\e^?' tcsh-backward-delete-word
+
---
+++ linux.zsh
@@ -0,0 +1,14 @@
+[ -z "$PS1" ] && return
+ACTIVE_SHRC=$ACTIVE_SHRC:'LINUX'   
+export ACTIVE_SHRC
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+#bindkey '5D' emacs-backward-word
+bindkey ';5D' emacs-backward-word
+#bindkey '5C' emacs-forward-word
+bindkey ';5C' emacs-forward-word
+
